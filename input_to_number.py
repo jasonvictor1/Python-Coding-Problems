8d5d630aec9c00644ac04f3922b7d586fe821886
Jason Victor <141011930+jasonvictor1@users.noreply.github.com>
2023-09-20 00:39:46 -0700

    User input to Number

---
+++ input_to_number.py
@@ -0,0 +1,11 @@
+# Prompt the user for an integer number and store the input as an integer
+int_num = int(input('Give me an integer number: '))
+
+# Prompt the user for a floating point number and store the input as a float
+float_num = float(input('Give me a float  number: '))
+
+# Multiply the integer number with the floating point number
+result = int_num * float_num
+
+# Display the result of the multiplication
+print('Your result is: ', result)
