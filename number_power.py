8be0f34ff7c9694aaab9d633c515290adf3dfc25
Jason Victor <141011930+jasonvictor1@users.noreply.github.com>
2023-09-22 00:47:13 -0700

    number power

---
+++ number_power.py
@@ -0,0 +1,11 @@
+# Prompting the user to provide the base number
+base_num = int(input('Give me the base number: '))
+
+# Prompting the user to provide the power/exponent
+power_num = int(input('Give me the power number: '))
+
+# Calculating the result by raising base_num to the power of power_num
+result = base_num ** power_num
+
+# Displaying the result to the user
+print('Your result is: ', result)
