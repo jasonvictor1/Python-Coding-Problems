579520ebf96ed1a51ea33e563e85c6c619d8c2f6
Jason Victor <141011930+jasonvictor1@users.noreply.github.com>
2023-09-28 00:42:38 -0700

    Create a random number

---
+++ random_number.py
@@ -0,0 +1,8 @@
+# Import the random module to generate random numbers
+import random
+
+# Generate a random integer between 0 and 10 (inclusive) and assign it to random_num
+random_num = random.randint(0, 10)
+
+# Print the randomly generated number to the console
+print(random_num)
