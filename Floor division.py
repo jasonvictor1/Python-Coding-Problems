ea14a3ab683c33a37021d4a92347b18965edf81e
Jason Victor <141011930+jasonvictor1@users.noreply.github.com>
2023-10-18 00:24:21 -0700

    finding the floor division of two numbers

---
+++ Floor division.py
@@ -0,0 +1,11 @@
+# Get the first number from the user and convert it to an integer
+num1 = int(input('Enter the first number: '))
+
+# Get the second number from the user and convert it to an integer
+num2 = int(input('Enter the second number: '))
+
+# Divide the first number by the second number and take the integer quotient (floor division)
+result = num1//num2
+
+# Print the result of the division
+print(result)
