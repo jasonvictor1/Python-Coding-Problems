63001ea0dbe3cf5a28f3d494b0ec02c1fa688645
Jason Victor <141011930+jasonvictor1@users.noreply.github.com>
2024-02-01 01:45:20 -0800

    updated comments for code

--- #2 Easy Ones/swap variables.py
+++ #2 Easy Ones/swap variables.py
@@ -1,9 +1,11 @@
+# Initialize two variables 'a' and 'b' with values 6 and 7, respectively
 a = 6
 b = 7
 
-
-# Swapping variables
+# Swapping the values of 'a' and 'b'
+# This is done in a pythonic way where 'a' gets the value of 'b' and 'b' gets the value of 'a'
 a, b = b, a
 
+# Print the values of 'a' and 'b' after swapping
 print("a =", a)
 print("b =", b)
